8f749d74f0e54cb553e7c4e2aaf1affd6495cee1
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 18:36:10 +0300

    9-20 Client API methods are added to methods.js.

--- methods.js
+++ methods.js
@@ -1,36 +1,107 @@
 var methods = [
+  // 1
   {
     "name": "showInstallBox"
   },
+  // 2
 	{
     "name": "showSettingsBox",
     "settings": 8214
   },
+  // 3
   {
     "name": "showGroupSettingsBox",
     "settings": 4096
   },
+  // 4
   {
     "name": "showRequestBox",
     "user_id": 123456789,
     "message": "Hello!",
     "requestKey": "myRequestKey"
   },
+  // 5
   {
     "name": "showInviteBox",
   },
+  // 6
   {
     "name": "showOrderBox",
     "type": "item",
     "item": "item_25new"
   },
+  // 7
   {
     "name": "showProfilePhotoBox",
     "photo_hash": "sdf87dfhsdfdfjererhfd9",
   },
+  // 8
   {
     "name": "resizeWindow",
     "width": 500,
     "height": 500
+  },
+  // 9
+  {
+    "name": "scrollWindow",
+    "top": 200,
+    "speed": 500
+  },
+  // 10
+  {
+    "name": "setTitle",
+    "title": "New title"
+  },
+  // 11
+  {
+    "name": "setLocation",
+    "location": "new_location",
+  },
+  // 12
+  {
+    "name": "scrollTop",
+  },
+  // 13
+  {
+    "name": "scrollSubscribe",
+    "fireEvent": true
+  },
+  // 14
+  {
+    "name": "showInstallPushBox",
+  },
+  // 15
+  {
+    "name": "showAllowMessagesFromCommunityBox",
+  },
+  // 16
+  {
+    "name": "callUser",
+    "user_id": 66748,
+    "call_id": "call1234",
+    "message": "Hi!"
+  },
+  // 17
+  {
+    "name": "checkUnitySupport",
+  },
+  // 18
+  {
+    "name": "checkFlashSupport",
+  },
+  // 19
+  {
+    "name": "showSubscriptionBox",
+    "action": "create",
+    "item": "subscription1"
+  },
+  // 20
+  {
+    "name": "showAppWidgetPreviewBox",
+    "type": "text",
+    "code": "return { 
+      "title": "Цитата", 
+      "text": "Текст цитаты" 
+    };"
   },
 ];
