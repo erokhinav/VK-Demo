5503a6e83d398aef54b9793440eff145e0fda5e5
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 18:12:49 +0300

    5-8 methods are added to methods.js.

--- methods.js
+++ methods.js
@@ -4,16 +4,33 @@ var methods = [
   },
 	{
     "name": "showSettingsBox",
-    "settings": "8214"
+    "settings": 8214
   },
   {
     "name": "showGroupSettingsBox",
-    "settings": "4096"
+    "settings": 4096
   },
   {
     "name": "showRequestBox",
-    "user_id": "123456789",
+    "user_id": 123456789,
     "message": "Hello!",
     "requestKey": "myRequestKey"
+  },
+  {
+    "name": "showInviteBox",
+  },
+  {
+    "name": "showOrderBox",
+    "type": "item",
+    "item": "item_25new"
+  },
+  {
+    "name": "showProfilePhotoBox",
+    "photo_hash": "sdf87dfhsdfdfjererhfd9",
+  },
+  {
+    "name": "resizeWindow",
+    "width": 500,
+    "height": 500
   },
 ];
