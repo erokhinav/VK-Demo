283a651695e9ac74f266b81699c29918947f74ab
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 17:29:33 +0300

    Dropdown list is added.

--- index.js
+++ index.js
@@ -8,29 +8,26 @@
      location.reload();
 }, '5.69');
 
-// var mydata = JSON.parse(methods);
-// console.log(mydata);
+var methodsSelect = document.getElementById("methodsSelect");
+console.log(methods[0]);
+for (var i = 0; i < methods.length; i++) {
+  var newEl = document.createElement("option");
+  newEl.text = methods[i].name;
+  methodsSelect.options.add(newEl, i + 1);
+}
 
-// var methodsSelect = document.getElementById("methodsSelect");
-// console.log(methods[0]);
-// for (var i = 0; i < methods.length; i++) {
-//   var newEl = document.createElement("option");
-//   newEl.text = methods[i].name;
-//   methodsSelect.options.add(newEl, i + 1);
-// }
-
-// function callMethod() {
-//   var selInd = methodsSelect.selectedIndex;
-//   console.log(selInd);
-//   var method = methods[selInd];
-//   var props = [];
-//   Object.keys(method).forEach(function eachKey(key) {
-//     props.push(method[key]);
-//   });
-//   console.log(props);
-//   VK.callMethod(method.name);
-//   VK.callMethod.apply(this, props);
-// }
+function callMethod() {
+  var selInd = methodsSelect.selectedIndex;
+  console.log(selInd);
+  var method = methods[selInd];
+  var props = [];
+  Object.keys(method).forEach(function eachKey(key) {
+    props.push(method[key]);
+  });
+  console.log(props);
+  // VK.callMethod(method.name);
+  VK.callMethod.apply(this, props);
+}
 
 // VK.addCallback('onWindowBlur', function f() {
 //             alert("!!!");
