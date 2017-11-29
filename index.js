28f958c83461a1d07dabf9d4f63e61cd5ef9e43f
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 17:23:54 +0300

    test commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   VK.init(function() {
      // API initialization succeeded
      console.log("!");
-     VK.callMethod("showSettingsBox", 8214);
+     // VK.callMethod("showSettingsBox", 8214);
   }, function() {
      // API initialization failed
      location.reload();
@@ -11,31 +11,31 @@
 // var mydata = JSON.parse(methods);
 // console.log(mydata);
 
-var methodsSelect = document.getElementById("methodsSelect");
-console.log(methods[0]);
-for (var i = 0; i < methods.length; i++) {
-  var newEl = document.createElement("option");
-  newEl.text = methods[i].name;
-  methodsSelect.options.add(newEl, i + 1);
-}
+// var methodsSelect = document.getElementById("methodsSelect");
+// console.log(methods[0]);
+// for (var i = 0; i < methods.length; i++) {
+//   var newEl = document.createElement("option");
+//   newEl.text = methods[i].name;
+//   methodsSelect.options.add(newEl, i + 1);
+// }
 
-function callMethod() {
-  var selInd = methodsSelect.selectedIndex;
-  console.log(selInd);
-  var method = methods[selInd];
-  var props = [];
-  Object.keys(method).forEach(function eachKey(key) {
-    props.push(method[key]);
-  });
-  console.log(props);
-  VK.callMethod(method.name);
-  VK.callMethod.apply(this, props);
-}
+// function callMethod() {
+//   var selInd = methodsSelect.selectedIndex;
+//   console.log(selInd);
+//   var method = methods[selInd];
+//   var props = [];
+//   Object.keys(method).forEach(function eachKey(key) {
+//     props.push(method[key]);
+//   });
+//   console.log(props);
+//   VK.callMethod(method.name);
+//   VK.callMethod.apply(this, props);
+// }
 
-VK.addCallback('onWindowBlur', function f() {
-            alert("!!!");
-        });
-VK.removeCallback('onWindowBlur');
+// VK.addCallback('onWindowBlur', function f() {
+//             alert("!!!");
+//         });
+// VK.removeCallback('onWindowBlur');
 
 function myFunction() {
   console.log(document.getElementById("methodsSelect").selectedIndex);
