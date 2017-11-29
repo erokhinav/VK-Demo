25c392329eb18cc2a11ef1dd77e36dcc9f52dbf6
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 17:02:42 +0300

    Dropdown list with Client API methods is added.

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-  // window.name = 'fXD';
+  window.name = 'fXD';
   VK.init(function() {
      // API initialization succeeded
      console.log("!");
-     // document.getElementById("demo").innerHTML = "test!";
   }, function() {
      // API initialization failed
-     // Can reload page here
+     location.reload();
 }, '5.69');
 
-  
+// var mydata = JSON.parse(methods);
+// console.log(mydata);
 
-var x = document.getElementById("mySelect");
-var c = document.createElement("option");
-c.text = "Kiwi";
-x.options.add(c, 1);
+var methodsSelect = document.getElementById("methodsSelect");
+console.log(methods[0]);
+for (var i = 0; i < methods.length; i++) {
+  var newEl = document.createElement("option");
+  newEl.text = methods[i].name;
+  methodsSelect.options.add(newEl, i + 1);
+}
 
+function callMethod() {
+  var selInd = methodsSelect.selectedIndex;
+  console.log(selInd);
+  var method = methods[selInd];
+  var props = [];
+  Object.keys(method).forEach(function eachKey(key) {
+    props.push(method[key]);
+  });
+  console.log(props);
+  VK.callMethod.apply(null, props);
+}
 
-
-VK.addCallback('onWindowBlur', function f(){
+VK.addCallback('onWindowBlur', function f() {
             alert("!!!");
         });
 VK.removeCallback('onWindowBlur');
 
 function myFunction() {
-   document.getElementById("op1").innerHTML = "Option111";
- 	 document.getElementById("demo").innerHTML = "Paragraph changed.";
- 	 VK.callMethod("showSettingsBox", 8214);
+  console.log(document.getElementById("methodsSelect").selectedIndex);
+  // document.getElementById("op1").innerHTML = "Option111";
+  // document.getElementById("demo").innerHTML = "Paragraph changed.";
+  VK.callMethod("showSettingsBox", 8214);
 }
