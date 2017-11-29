3cf919e1c1871a05fdb9105f8a3a119429e48dd2
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-11-29 18:02:10 +0300

    test commit

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function callMethod() {
     props.push(method[key]);
   });
   console.log(props);
-  VK.callMethod(method.name, method.settings);
-  //VK.callMethod.apply(this, props);
+  // VK.callMethod(method.name, method.settings);
+  VK.callMethod.apply(this, props);
 }
 
 // VK.addCallback('onWindowBlur', function f() {
