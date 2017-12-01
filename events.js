7df3e6c8c1e8ea96052aa11b6fc76aeccedcb4fd
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-12-01 16:27:11 +0300

    Events list with checkboxes is added.
    Creating events.js file. Adding list of checkboxes at html file by
    filling it with content from events.js. Creating a function for
    handling clicking on checkboxes.

---
+++ events.js
@@ -0,0 +1,74 @@
+var events = [
+  // 1
+  {
+    "name": "onApplicationAdded"
+  },
+  // 2
+	{
+    "name": "onSettingsChanged",
+  },
+  // 3
+  {
+    "name": "onRequestSuccess",
+  },
+  // 4
+  {
+    "name": "onRequestCancel",
+  },
+  // 5
+  {
+    "name": "onRequestFail",
+  },
+  // 6
+  {
+    "name": "onBalanceChanged",
+  },
+  // 7
+  {
+    "name": "onOrderCancel",
+  },
+  // 8
+  {
+    "name": "onOrderSuccess",
+  },
+  // 9
+  {
+    "name": "onOrderFail",
+  },
+  // 10
+  {
+    "name": "onProfilePhotoSave",
+  },
+  // 11
+  {
+    "name": "onWindowResized",
+  },
+  // 12
+  {
+    "name": "onLocationChanged",
+  },
+  // 13
+  {
+    "name": "onWindowBlur",
+  },
+  // 14
+  {
+    "name": "onWindowFocus",
+  },
+  // 15
+  {
+    "name": "onScrollTop",
+  },
+  // 16
+  {
+    "name": "onScroll",
+  },
+  // 17
+  {
+    "name": "onToggleFlash",
+  },
+  // 18
+  {
+    "name": "onInstallPushSuccess",
+  },
+];
--- index.js
+++ index.js
@@ -1,34 +1,80 @@
-  // window.name = 'fXD';
-  VK.init(function() {
+  window.name = 'fXD';
+VK.init(function() {
      // API initialization succeeded
-     console.log("!");
+    console.log("!");
      // VK.callMethod("showSettingsBox", 8214);
-  }, function() {
+}, function() {
      // API initialization failed
-     location.reload();
+    location.reload();
 }, '5.69');
 
 
 var methodsSelect = document.getElementById("methodsSelect");
-console.log(methods);
+console.log(methodsSelect);
 console.log(methods[0]);
 for (var i = 0; i < methods.length; i++) {
-  var newEl = document.createElement("option");
-  newEl.text = methods[i].name;
-  methodsSelect.options.add(newEl, i + 1);
+    var newEl = document.createElement("option");
+    newEl.text = methods[i].name;
+    methodsSelect.options.add(newEl, i + 1);
 }
 
 function callMethod() {
-  var selInd = methodsSelect.selectedIndex;
-  console.log(selInd);
-  var method = methods[selInd];
-  var props = [];
-  Object.keys(method).forEach(function eachKey(key) {
-    props.push(method[key]);
-  });
-  console.log(props);
-  VK.callMethod.apply(this, props);
+    var selInd = methodsSelect.selectedIndex;
+    console.log(selInd);
+    var method = methods[selInd];
+    var props = [];
+    Object.keys(method).forEach(function eachKey(key) {
+        props.push(method[key]);
+    });
+    console.log(props);
+    VK.callMethod.apply(this, props);
 }
+
+var eventsList = document.getElementById('eventList');
+for (var i = 0; i < events.length; i++) {
+    var eventName = events[i].name;
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    // checkbox.name = "name";
+    checkbox.id = eventName;
+    var name = (" " + eventName).slice(1);
+    checkbox.onclick = createOnClickEvent(name);
+
+    var label = document.createElement('label')
+    // label.htmlFor = "id";
+    label.appendChild(document.createTextNode(eventName));
+
+    var li = document.createElement("li");
+    li.appendChild(checkbox);
+    li.appendChild(label);
+
+    eventsList.appendChild(li);
+    // eventsList.appendChild(label);
+}
+
+function createOnClickEvent(name) {
+    return function() {
+        listenEvent(name);
+    }
+}
+
+function listenEvent(eventName) {
+    console.log(eventName);
+    if (document.getElementById(eventName).checked) {
+        VK.addCallback(eventName, function f() {
+            alert(args);
+        });
+    } else {
+        VK.removeCallback(eventName);
+    }
+}
+
+
+
+// var eventsList = document.getElementById('eventList');
+// console.log(eventsList);
+// eventsList.appendChild(checkbox);
+// eventsList.appendChild(label);
 
 // VK.addCallback('onWindowBlur', function f() {
 //             alert("!!!");
